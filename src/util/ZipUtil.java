dc53bc263423e816d8d0931825e17931ee34b339
dcmgl <dcmgl@126.com>
2016-11-14 17:39:55 +0800

    zip
    zip tool

--- src/util/ZipUtil.java
+++ src/util/ZipUtil.java
@@ -3,8 +3,13 @@ package util;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.CRC32;
+import java.util.zip.CheckedOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class ZipUtil {
 
@@ -13,7 +18,13 @@ public class ZipUtil {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			ZipUtil.zip("C:\\GitHub\\MyAutoTest\\test-output",
+					"C:\\GitHub\\MyAutoTest", "test.zip");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	private static void zip(String srcRootDir, File file, ZipOutputStream zos)
@@ -40,13 +51,64 @@ public class ZipUtil {
 				zos.write(data, 0, count);
 			}
 			bis.close();
-			zos.close();
-		}else{
-			File[]childFileList = file.listFiles();
-			for(int n=0;n<childFileList.length;n++){
-				zip(srcRootDir,childFileList[n],zos);
+			zos.closeEntry();
+			
+		} else {
+			File[] childFileList = file.listFiles();
+			for (int n = 0; n < childFileList.length; n++) {
+				childFileList[n].getAbsolutePath().indexOf(
+						file.getAbsolutePath());
+				zip(srcRootDir, childFileList[n], zos);
 			}
+			
 		}
+
+	}
+
+	public static void zip(String srcPath, String zipPath, String zipFileName)
+			throws Exception {
+		if (StringUtils.isEmpty(srcPath) || StringUtils.isEmpty(zipFileName)
+				|| StringUtils.isEmpty(zipPath)) {
+			throw new Exception("file path is null!");
+		}
+		CheckedOutputStream cos = null;
+		ZipOutputStream zos = null;
+		try {
+			File srcFile = new File(srcPath);
+			if (srcFile.isDirectory() && zipPath.indexOf(srcPath) != -1) {
+				throw new Exception(
+						"zipPath must not be the child directory of srcPath.");
+			}
+
+			File zipDir = new File(zipPath);
+			if (!zipDir.exists() || !zipDir.isDirectory()) {
+				zipDir.mkdirs();
+			}
+
+			String zipFilePath = zipPath + File.separator + zipFileName;
+			File zipFile = new File(zipFileName);
+			if (zipFile.exists()) {
+				zipFile.delete();
+			}
+			cos = new CheckedOutputStream(new FileOutputStream(zipFile),
+					new CRC32());
+			zos = new ZipOutputStream(cos);
+
+			String srcRootDir = srcPath;
+			if (srcFile.isFile()) {
+				int index = srcPath.lastIndexOf(File.separator);
+				if (index != -1) {
+					srcRootDir = srcPath.substring(0, index);
+				}
+			}
+			zip(srcRootDir, srcFile, zos);
+			zos.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			zos.close();
+		}
+
 	}
 
 }
